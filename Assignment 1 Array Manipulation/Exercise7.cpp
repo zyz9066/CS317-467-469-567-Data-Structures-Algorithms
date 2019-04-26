417e0424327a05ab25246db11b8982d72c423b1b
Tianye Zhao <zyz9066@yahoo.com>
2019-04-26 16:37:17 -0400

    Update Exercise7.cpp

--- Assignment 1 Array Manipulation/Exercise7.cpp
+++ Assignment 1 Array Manipulation/Exercise7.cpp
@@ -1,7 +1,7 @@
 // Exercise7.cpp by Tianye Zhao
 // IDE: Dev-C++ 5.11
 // updated 2019-02-10
-#include "array.h"
+#include "Array.h"
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
@@ -18,7 +18,7 @@ int main( int argc, char ** argv ) {
 
         // Initialize array with random numbers
         for (int i = 0; i < 10; i++) {
-        	A.insertLast(rand() % 10);
+        	A.push_back(rand() % 10);
         }
         
         // Display array
